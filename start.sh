380264214860bcf9809f89f92059339bf777af57
Harshal Kulkarni <harshalk.91@gmail.com>
2021-07-11 23:00:24 +0530

    fix error and add check for camunda

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-docker-compose uo -d
+docker-compose up -d
+while true;
+do
+  response=$(curl -o /dev/null -s -w "%{http_code}\n" localhost:8080)
+  if [ $response -eq "200" ]
+  then
+    echo "camunda is ready."
+    exit 0
+  else
+    echo "Please wait.. camunda is initializing.."
+    sleep 2
+  fi
+done
